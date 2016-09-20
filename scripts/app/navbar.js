fd09e865ee44d0691e78cd76a1909a2804de642d
Jeff Schwartz <jefftschwartz@gmail.com>
2016-09-20 12:26:30 -0400

    Navbar now expands and contracts.

---
+++ scripts/app/navbar.js
@@ -0,0 +1,19 @@
+let Navbar = (function () {
+    let navbarEl = document.getElementsByClassName("navbar")[0];
+    console.log("navbarEl", navbarEl);
+    let navbarCollapseIconEl =
+        document.getElementsByClassName("navbar__collapse-icon")[0];
+    console.log("navbarCollapseIconEl", navbarCollapseIconEl);
+    let navbarNavItems = document.getElementsByClassName("navbar__nav-items")[0];
+    console.log("navbarNavItems", navbarNavItems);
+    navbarCollapseIconEl.addEventListener("click", function (e) {
+        console.log("icon clicked");
+        if (navbarNavItems.style.display === "none") {
+            navbarNavItems.style.display = "block";
+        } else {
+            navbarNavItems.style.display = "none";
+        }
+    });
+}());
+
+export default Navbar;
