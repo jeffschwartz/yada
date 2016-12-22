c0662787fc1811cc26020db496b3d11a05c1313d
Jeff Schwartz <jefftschwartz@gmail.com>
2016-12-22 07:39:22 -0500

    Turned carousel automation back on.

--- scripts/app/css.js
+++ scripts/app/css.js
@@ -1,3 +1,2 @@
 // imports for side effects only
 import "./carousel";
-import tab from "./tab";
--- scripts/app/yada.js
+++ scripts/app/yada.js
@@ -1,12 +1,12 @@
+import * as Yada from "./css";
 import registerTabBar from "./tab";
-
-// import * as Yada from "./css";
 
 window.addEventListener("load", function (e) {
     // play the carousel - it returns a func that when called stops the carousel
-    // let carouselEl = document.getElementById("carousel1");
-    // carouselEl.carousel.play(0);
+    let carouselEl = document.getElementById("carousel1");
+    carouselEl.carousel.play(0);
 
+    // register tab bar
     let elTabBar = document.getElementsByClassName("tab-bar")[0];
     console.log("elTabBar=", elTabBar);
     let elTabBarTab = elTabBar.querySelector("a.tab-bar-tab__label[href='#tabbarpane1']").parentElement;
